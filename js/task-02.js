a986a307274d7ce0512281951c03793bedb39ed1
SergeyZhydkov <134008114+SergeyZhydkov@users.noreply.github.com>
2023-09-27 21:43:48 +0300

    доопрацювання

--- js/task-02.js
+++ js/task-02.js
@@ -1,28 +1,9 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
-// console.log(ingredients);
-const list = document.querySelector("#ingredients");
-const potatoes = document.createElement("li");
-potatoes.textContent = "Potatoes";
+const productList = document.getElementById("ingredients");
 
-const mushrooms = document.createElement("li");
-mushrooms.textContent = "Mushrooms";
-const garlic = document.createElement("li");
-garlic.textContent = "Garlic";
-const tomatos = document.createElement("li");
-tomatos.textContent = "Tomatos";
-const herbs = document.createElement("li");
-herbs.textContent = "Herbs";
-const condiments = document.createElement("li");
-condiments.textContent = "Condiments";
-
-// console.log(potatoes);
-// console.log(mushrooms);
-// console.log(garlic);
-// console.log(tomatos);
-// console.log(herbs);
-// console.log(condiments);
-
-const markup = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
-// console.log(markup);
-list.innerHTML = markup;
-console.log(list);
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  productList.appendChild(listItem);
+});
